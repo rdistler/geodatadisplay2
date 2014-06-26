1e0ca97af59c44af0161d6bc2fc5a1012c42e7b8
Rodney Distler <rodney.distler@oa.mo.gov>
2014-06-26 10:31:17 -0500

    add datagrid directive

---
+++ app/scripts/directives/datagrid.js
@@ -0,0 +1,9 @@
+geodatadisplay.directive('datagrid', function () {
+  var linker = function (scope, element, attrs) {
+  	console.log('Executing Linker function for datagrid directive');
+
+  };
+  return {
+    restrict: 'E',
+    link: linker
+}; });
